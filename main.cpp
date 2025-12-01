bc39e73c181bca55a00a7f409fbbf34b6a5e7cac
joseph <joeyryan8888@gmail.com>
2025-12-01 10:18:49 -0600

    e

--- main.cpp
+++ main.cpp
@@ -11,17 +11,17 @@ class io {
             {
                 int i = 0;
             
-            std::cout <<  << "x x x x" << std::endl;
-            std::cout << "x     x" << std::endl;
-            std::cout << "x     x" << std::endl;
-            std::cout << "x x x x" << std::endl;
+            std::cout << spacesin(i); << "x x x x" << std::endl;
+            std::cout << spacesin(i); << "x     x" << std::endl;
+            std::cout << spacesin(i); << "x     x" << std::endl;
+            std::cout << spacesin(i); << "x x x x" << std::endl;
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
             system("clear");
             
-            std::cout << "       " << std::endl;
-            std::cout << "  x x  " << std::endl;
-            std::cout << "  x x  " << std::endl;
-            std::cout << "       " << std::endl;
+            std::cout << spacesin(i); << "       " << std::endl;
+            std::cout << spacesin(i); << "  x x  " << std::endl;
+            std::cout << spacesin(i); << "  x x  " << std::endl;
+            std::cout << spacesin(i); << "       " << std::endl;
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
             system("clear");
             i++;
